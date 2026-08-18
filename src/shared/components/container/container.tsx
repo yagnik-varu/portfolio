71478748a9cc6eecd489dfffba74595bad84bcc2
Yagnik <yagnikvaru2003@gmail.com>
2026-08-18 15:35:38 +0530

    fix: Typescript error of interface on empty object by declare type

--- src/shared/components/container/container.tsx
+++ src/shared/components/container/container.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils/cn";
 
-export interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
+export type ContainerProps = React.HTMLAttributes<HTMLDivElement>;
 
 const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
   ({ className, ...props }, ref) => {
